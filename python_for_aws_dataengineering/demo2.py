5f90f0a504fef782467f911b67fdf1cd153ca4e9
cfamilyit <96969060+cfamilyit@users.noreply.github.com>
2023-04-26 06:53:42 +0530

    Add files via upload

---
+++ python_for_aws_dataengineering/demo2.py
@@ -0,0 +1,4 @@
+from python_packages_example import arithmetic as ar
+print("A is:",ar.a)
+print("*"*30)
+ar.sum()
---
+++ python_for_aws_dataengineering/main.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+res = a + b
+print('A is:',a)
+print('B is:',b)
+print('Sum of A,B is:',res)
